c077afcbee9f6f765cedc886379d345716f1caac
rishikesh-shukla <rishikesh.shukla@einfochips.com>
2020-02-24 15:06:32 +0530

    Correction

--- paypal.py
+++ paypal.py
@@ -6,8 +6,7 @@ app = Flask(__name__)
 
 paypalrestsdk.configure({
   "mode": "sandbox", # sandbox or live
-  "client_id": "AVEFNPNY_6BX6KFKXdaNL6fXPoU_PBkEq4KN_TQnTRkVkBgcOYEqjHKMJYOmgy9dPGq_9Rvu6XGmEkp_",
-  "client_secret": "EDy-Uv1gsdy91EfJIP-rKELABm-9D9NUUYUSshb5z8HkEqMc2TAirB94l2A1262Rx1obsz1VL_awRPtD" })
+  
 
 @app.route('/')
 def index():
